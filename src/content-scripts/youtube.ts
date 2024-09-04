4b21cab51f627ac3e1939a102837b52ff07d5beb
hariprasadr1hp <hariprasadr1hp@tutanota.com>
2024-09-04 09:13:12 +0200

    adding timeout to  yt transcript func

--- src/content-scripts/youtube.ts
+++ src/content-scripts/youtube.ts
@@ -1,17 +1,26 @@
 import { MessageID } from "@/models/message-types";
+import { sleep } from "@/util/util";
 
+/**
+  * Returns the youtube link in an ".org" recognized format
+  * Along with the link, the title and the owner of the video is extracted
+  * format: [[{link}][yt:{owner}-{title}]]
+  */
 function getYTOrgLink(): string {
   const link = document.location.href;
   const titleElement = document.querySelector("#title yt-formatted-string");
+  const ownerElement = document.querySelector("#owner a.yt-simple-endpoint.style-scope.yt-formatted-string");
   const title = titleElement ? titleElement.innerHTML : "";
-  return `[[${link}][${title}]]`;
+  const owner = ownerElement ? ownerElement.innerHTML : "";
+  return `[[${link}][yt:${owner}-${title}]]`;
 }
 
-function getYTTranscript(): string {
+async function getYTTranscript(): Promise<string> {
   const expandButton = document.querySelector<HTMLElement>("tp-yt-paper-button#expand");
   if (expandButton) {
     expandButton.click();
   }
+  await sleep(1);
 
   const transcriptButton = document.querySelector<HTMLButtonElement>("button[aria-label='Show transcript']");
   if (transcriptButton) {
@@ -19,6 +28,7 @@ function getYTTranscript(): string {
   } else {
     return "Transcript Button NOT FOUND!";
   }
+  await sleep(1);
 
   const transcriptElements = document.querySelectorAll<HTMLElement>(
     "ytd-transcript-segment-list-renderer yt-formatted-string"
@@ -33,8 +43,8 @@ chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
   }
 })
 
-chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(async (message, _sender, sendResponse) => {
   if (message.id === MessageID.YT_VIDEO_TRANSCRIPT) {
-    sendResponse(getYTTranscript());
+    sendResponse(await getYTTranscript());
   }
 })
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,3 +1,7 @@
 export async function checkHttpProtocol(document: Document): Promise<boolean> {
-    return (!["http:", "https:"].includes(document.location.protocol))
+  return (!["http:", "https:"].includes(document.location.protocol))
+}
+
+export function sleep(seconds: number) {
+  return new Promise(resolve => setTimeout(resolve, seconds * 1000))
 }
